9264c061ae127a2e788b2bd190ed2d48c7ef11b7
jackson angerdina <jackson@angerdina.com>
2016-01-21 08:41:25 -0600

    Joystick code didn't push

---
+++ src/org/usfirst/frc2992/Robot/mhJoystick.java
@@ -0,0 +1,87 @@
+package org.usfirst.frc2992.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class mhJoystick extends Joystick{
+
+	public mhJoystick(int port) {
+		super(port);
+		// TODO Auto-generated constructor stub
+	};
+	public mhJoystick(int port, int numAxisTypes, int numButtonTypes){
+		super(port, numAxisTypes, numButtonTypes);
+	}
+	
+	/*
+	 * get x axis with smoothing
+	 */
+	public double smoothGetX(){
+		double val = super.getX();
+		return smoothPowerCurve(deadzone(val, .1));
+	}
+	
+	/*
+	 * get y axis with smoothing
+	 */
+	public double smoothGetY(){
+		double val = super.getY();
+		return smoothPowerCurve(deadzone(val, .1));
+	}
+	
+	
+	/* This does the cubic smoothing equation on joystick value.
+	 * Assumes you have already done any deadzone processing.
+	 * 
+	 * @param x  joystick input
+	 * @return  smoothed value
+	 */
+	protected double smoothPowerCurve (double x) {
+        //double a = this.getThrottle();
+        double a = 1.0;         // Hard code to max smoothing 
+        double b = 0.15;
+        
+        if (x > 0.0)
+            return (b + (1.0-b)*(a*x*x*x+(1.0-a)*x));
+        
+        else if (x<0.0)
+            return (-b + (1.0-b)*(a*x*x*x+(1.0-a)*x));
+        else return 0.0;
+    }
+	
+	
+	/*
+	 * adds deadzone to joysticks
+	 * 
+	 * @param rawStick Raw value from joystick read -1.0 to 1.0
+	 * @param dz	Deadzone value to use 0 to 0.999
+     * @return		Value after deadzone processing
+	 */
+	 protected double deadzone(double rawStick, double dz) {
+	        double stick;
+
+	        // Force limit to -1.0 to 1.0
+	        if (rawStick > 1.0) {
+	            stick = 1.0;
+	        } else if (rawStick < -1.0) {
+	            stick = -1.0;
+	        } else {
+	            stick = rawStick;
+	        }
+
+	        // Check if value is inside the dead zone
+	        if (stick >= 0.0){
+	            if (Math.abs(stick) >= dz) 
+	                return (stick - dz)/(1 -  dz);
+	            else 
+	                return 0.0;
+	            
+	        }
+	        else {
+	            if (Math.abs(stick) >= dz) 
+	                return (stick + dz)/(1 - dz);
+	            else 
+	                return 0.0;
+	            
+	        }
+	 }
+}
